a97ea14abdb6e2c2affaa49dc7a885ed1792195b
LunarSphere <supermax309@gmail.com>
2024-07-11 16:16:51 -0500

    uav custom env is good to go

--- UAV_env_custom.py
+++ UAV_env_custom.py
@@ -13,6 +13,7 @@ class UAVEnv(gym.Env):
         #important values for mobile edge server 
         self.uav_position = np.array([0.0, 0.0], dtype=np.float32) #x, y position of the UAV
         self.height = 100 #height of the UAV
+        self.area = 100 #axa area so this is legnth and width of the area
         self.sum_task_size = 100 * 1048576
         self.bandwidth_nums = 1 
         self.Bandwidth = self.bandwidth_nums * 10 * 6 #Bandwidth 1MHz
@@ -57,7 +58,7 @@ class UAVEnv(gym.Env):
         self.start_state = self.state # the changes I implemented have changed the output of the state. to not be float 32 7/10/2024
         #print(self.state) debugging delete later
         #decided to change action space to normalize values between -1 and 1 and have a tuple with discrete number of users. 
-        self.action_space = spaces.Box(low=-1, high=1, shape=(4,), dtype=np.float32)
+        self.action_space = spaces.Box(low=-1, high=1, shape=(5,), dtype=np.float32)
         
         self.observation_space = spaces.Dict({
             'battery_remaining': spaces.Box(low=0, high=self.e_battery_uav, shape=(1,), dtype=np.float32),
@@ -126,31 +127,80 @@ class UAVEnv(gym.Env):
     #should spend some time thinking about novelty. 
     def step(self,action):
         step_redo = False
-        is_terminal = False
+        terminated = False
         offloading_ratio_change = False
         reset_dist = False
+
         # get values from the action
-        angle_action = action[0]
-        flight_distance_action = action[1]
-        climb_angle_action = action[2]
-        user_selection_action = action[3]
+        angle = action[0]
+        flight_distance = action[1]
+        climb_angle = action[2]
+        user_selection = action[3]
+        offloading_ratio = (action[4] + 1) /2
         
-        # Denormalize continuous actions
-        angle = (angle_action + 1) * 180  # Map [-1, 1] to [0, 360]
-        flight_distance = (flight_distance_action + 1) * 49.5  # Map [-1, 1] to [0, 99]
-        climb_angle = climb_angle_action * 45  # Assuming climb angle range is [-45, 45] degrees
+        # convert to actual values
+        angle = (angle + 1) * 180  # Map [-1, 1] to [0, 360]
+        flight_distance = (flight_distance + 1) * 49.5  # Map [-1, 1] to [0, 99]
+        climb_angle = climb_angle * 45  # Assuming climb angle range is [-45, 45] degrees
         
         # Convert user selection action to a discrete integer in the range [0, max_users - 1]
-        user_selection = int((user_selection_action + 1) * (self.users - 1) / 2)
-        
-        
+        user_selection = int((user_selection + 1) * (self.users - 1) / 2)
+        task_size = self.task_list[user_selection]
+        block_flag = self.block_flag_list[user_selection]
 
+        #calculate chosen flight distance
+        dis_fly = flight_distance * self.flight_speed * self.t_fly
+        #calculate energy consumption
+        e_fly = (dis_fly/self.t_fly) ** 2 * self.m_uav *  self.t_fly * 0.5
+        #calculate post flight position of uav
+        dx_uav = dis_fly * math.cos(angle)
+        dy_uav = dis_fly * math.sin(angle)
+        loc_uav_post_x = self.uav_position[0] + dx_uav
+        loc_uav_post_y = self.uav_position[1] + dy_uav
+        
+        #calculate server computing energy consumption
+        t_server = offloading_ratio * task_size / (self.f_uav / self.s) 
+        e_server = self.r * self.s ** 3 * t_server
+
+        #logic to determine how step progresses. 
+        if self.sum_task_size == 0:
+            terminated = True
+            reward = 0
+        elif self.sum_task_size - self.task_list[user_selection] < 0:
+            self.task_list = np.ones(self.users) * self.sum_task_size
+            reward = 0
+            step_redo = True
+        elif loc_uav_post_x < 0 or loc_uav_post_x > self.area or loc_uav_post_y < 0 or loc_uav_post_y > self.area:  # uav position is wrong:
+            reset_dist = True
+            ue_coords = [self.loc_ue_list[user_selection*2], self.loc_ue_list[(user_selection*2)+1]]
+            delay = self.com_delay(ue_coords, self.uav_position, offloading_ratio, task_size, block_flag)
+            reward = -delay
+            # Update status at next moment
+            self.e_battery_uav = self.e_battery_uav - e_server  # uav remaining power
+            self.reset2(delay, self.uav_position[0], self.uav_position[1], offloading_ratio, task_size, user_selection)
+        elif self.e_battery_uav < e_fly or self.e_battery_uav - e_fly < e_server:  # uav power cannot support calculations
+            ue_coords = [self.loc_ue_list[user_selection*2], self.loc_ue_list[(user_selection*2)+1]]
+            delay = self.com_delay(ue_coords, self.uav_position, offloading_ratio, task_size, block_flag)
+            reward = -delay
+            # Update status at next moment
+            self.e_battery_uav = self.e_battery_uav - e_server  # uav remaining power
+            self.reset2(delay, self.uav_position[0], self.uav_position[1], 0, task_size, user_selection)
+            offloading_ratio_change = True
+        else:
+            ue_coords = [self.loc_ue_list[user_selection*2], self.loc_ue_list[(user_selection*2)+1]]
+            delay = self.com_delay(ue_coords, self.uav_position, offloading_ratio, task_size, block_flag)
+            reward = -delay
+            # Update status at next moment
+            self.e_battery_uav = self.e_battery_uav - e_fly - e_server  # uav remaining power
+            self.uav_position[0] = loc_uav_post_x
+            self.uav_position[1] = loc_uav_post_y
+            self.reset2(delay, loc_uav_post_x, loc_uav_post_y, offloading_ratio, task_size, user_selection)
         #below is necessary for the environment to work with stable baselines 3 but I will need to change it later
         reward = 1 if self.uav_position[0] == 0 else 0
-        terminated = False
-        truncated = False
+        
+        truncated = False #means step ended due to a time limit
         observation = self.state
-        info = {}
+        info = {"redo_step": step_redo, "reset_dist": reset_dist, "offloading_ratio_change": offloading_ratio_change}
         return (observation, reward, terminated, truncated, info)
     
 
@@ -164,12 +214,35 @@ class UAVEnv(gym.Env):
     #above this point the methods are necessary to pass the stable baselines check
      
      
-     
-     
-       
+      #Reset ue task size, remaining total task size, ue position, and record to file
+    def reset2(self, delay, x, y, offloading_ratio, task_size, user_selection):
+        #ue_coords = [self.loc_ue_list[user_selection*2], self.loc_ue_list[(user_selection*2)+1]]
+        self.sum_task_size -= self.task_list[user_selection]  # Remaining tasks
+        for i in range(self.users):  #ue position after random movement
+            tmp = np.random.rand(2)
+            theta_ue = tmp[0] * np.pi * 2  #ue Random movement angle
+            dis_ue = tmp[1] * self.delta_t * self.v_ue  #ue random movement distance
+            self.loc_ue_list[user_selection*2] = self.loc_ue_list[user_selection*2] + math.cos(theta_ue) * dis_ue
+            self.loc_ue_list[(user_selection*2)+1] = self.loc_ue_list[(user_selection*2)+1] + math.sin(theta_ue) * dis_ue
+            self.loc_ue_list[i] = np.clip(self.loc_ue_list[i], 0, self.area)
+        #self.reset_step()  # ue random computing task 1~2Mbits # 4 ue, ue occlusion situation
+        #where reset step would be
+        self.task_list = np.random.randint(2097153, 2621440, self.users)  # Random computing task 2~2.5Mbits -> 80
+        self.block_flag_list = np.random.randint(0,2,self.users,dtype=np.int8) #determine which uavs are blocked or not in line of sight of the drone randomly.
+        # Record UE expenses
+        file_name = 'output.txt'
+        # file_name = 'output_ddpg_' + str(self.bandwidth_nums) + 'MHz.txt'
+        with open(file_name, 'a') as file_obj:
+            file_obj.write("\nUE-" + '{:d}'.format(user_selection) + ", task size: " + '{:d}'.format(int(task_size)) + ", offloading ratio:" + '{:.2f}'.format(offloading_ratio))
+            file_obj.write("\ndelay:" + '{:.2f}'.format(delay))
+            file_obj.write("\nUAV hover loc:" + "[" + '{:.2f}'.format(x) + ', ' + '{:.2f}'.format(y) + ']')  # Output retains two digits of the result
+
+           
     # Calculate cost
     #loc_uav == to uav_position and loc_ue == to ue_position
     def com_delay(self, loc_ue, loc_uav, offloading_ratio, task_size, block_flag):
+        #print(loc_uav)
+        #print (loc_ue) #debugging uav position is just a single value
         dx = loc_uav[0] - loc_ue[0]
         dy = loc_uav[1] - loc_ue[1]
         dh = self.height
@@ -178,7 +251,7 @@ class UAVEnv(gym.Env):
         if block_flag == 1:
             p_noise = self.p_noisy_nlos
         g_uav_ue = abs(self.alpha0 / dist_uav_ue ** 2)  #Channel gain
-        trans_rate = self.B * math.log2(1 + self.p_uplink * g_uav_ue / p_noise) # Uplink transmission rate bps
+        trans_rate = self.Bandwidth * math.log2(1 + self.p_uplink * g_uav_ue / p_noise) # Uplink transmission rate bps
         t_tr = offloading_ratio * task_size / trans_rate  # Upload delay, 1B=8bit
         t_edge_com = offloading_ratio * task_size / (self.f_uav / self.s)  # Calculate the delay on the UAV edge server
         t_local_com = (1 - offloading_ratio) * task_size / (self.f_ue / self.s)  # Local calculation delay
